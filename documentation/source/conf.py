e17dcc7791b37ea20e2298467e0a749632fd98e9
Lucas Vivier <79191733+lucas-vivier@users.noreply.github.com>
2021-11-23 16:57:39 +0100

    adding copyright for each .py file

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -30,7 +30,11 @@ release = '0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser', 'nbsphinx', 'sphinx.ext.githubpages',
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'myst_parser',
+              'nbsphinx',
+              'sphinx.ext.githubpages',
               'sphinxcontrib.bibtex']
 
 myst_enable_extensions = [
--- project/res_irf.py
+++ project/res_irf.py
@@ -1,11 +1,20 @@
-"""
-Res-IRF script
---------------
-
-
-This script requires that 'pandas' be installed within the Python
-environment you are running this script in.
-"""
+# Copyright 2020-2021 Ecole Nationale des Ponts et Chaussées
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Original author Lucas Vivier <vivier@centre-cired.fr>
+# Based on a scilab program mainly by written by Someone, but fully rewritten.
 
 import os
 import time
